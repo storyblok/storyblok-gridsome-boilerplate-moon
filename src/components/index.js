685ec0e9a758b0bf911344f9e4c8c1cc48e70903
ademarCardoso <ac@storyblok.com>
2020-01-21 17:03:02 -0300

    remove the old component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Logo from './Logo.vue'
 import Page from './Page.vue'
 import Link from './Link.vue'
-import Footer from './Footer.vue'
 import Sitemap from './Sitemap.vue'
 import Teaser from './Teaser.vue'
 import Metrics from './Metrics.vue'
