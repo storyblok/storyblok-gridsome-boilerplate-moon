c5f2fb909b2af048e85aaf02f7348d317cf01c22
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-02-06 11:35:08 -0300

    add nprogress

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import DefaultLayout from '~/layouts/Default.vue'
 import Components from './components'
 
@@ -8,11 +10,24 @@ import './assets/css/reset.css'
 import './assets/sass/global.scss'
 
 export default function (Vue, { router, head, isClient }) {
+  NProgress.configure()
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
   // registering the components
   Object.keys(Components).forEach(key => {
     Vue.component(key, Components[key])
+  })
+
+  router.beforeEach((_, from, next) => {
+    if (from.name !== null) {
+      NProgress.start()
+    }
+    next()
+  })
+      
+  router.afterEach(() => {
+    NProgress.done()
   })
 }
